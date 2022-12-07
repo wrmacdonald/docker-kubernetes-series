e29cec9fe39c3e6dc215c900e552329380053b37
Chris Thompson <chris.thompson2@insight.com>
2022-12-06 16:46:14 -0800

    add loop to test request

--- test_request.py
+++ test_request.py
@@ -1,6 +1,10 @@
 import requests
+import time
 
-url = "http://localhost:8000/ping"
-r = requests.get(url)
-print()
-print(r.text + "\n")
+url = "http://localhost:8000/hostname"
+for i in range(5):
+    r = requests.get(url)
+    print()
+    print(r.text + "\n")
+    time.sleep(2)
+
