66bb9a657f2ca9bfd2845bb1f2a2a8f57def2f26
Chris Thompson <chris.thompson2@insight.com>
2023-04-11 11:17:50 -0700

    isolate only Section 2 files

--- Section 3 - Scaling with Kubernetes/postgres/run_postgres.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env zsh
-
-docker run -d --rm \
-    --env-file=postgres.env \
-    -v "$(pwd)"/csvs:/var/lib/postgresql/csvs \
-    -v "$(pwd)"/scripts/init.sql:/docker-entrypoint-initdb.d/init.sql \
-    -p 5432:5432 \
-    --name postgres \
-    postgres:latest
