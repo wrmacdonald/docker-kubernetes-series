5e6d46bca8779b9a7a8797acdcd9e0d72f5a9d2d
Chris Thompson <chris.thompson2@insight.com>
2023-04-11 11:15:38 -0700

    delete section 2 and 3

--- Section 3 - Scaling with Kubernetes/predict/app/core/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from pydantic import BaseSettings
-
-class Settings(BaseSettings):
-
-    test_window: int = 30
-    date_column_name: str ='measurementdate'
-    predict_col: str ='tempmax'
